ef428ae975b7765d3006f8b82355a069b8c3e85c
QooraPL <mikiotwock@gmail.com>
2025-01-11 13:40:37 +0100

    Update TestNullpointer.java

--- src/TestNullpointer.java
+++ src/TestNullpointer.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class TestNullpointer {
+
+    public static void main(String[] args) {
+
+        try {
+            String s = null;
+            System.out.println(s.length());
+        } catch (NullPointerException e) {
+            System.out.println("Error catched: " + e.getMessage());
+        }
+    }
 }
