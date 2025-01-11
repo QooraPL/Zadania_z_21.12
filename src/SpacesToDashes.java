11fc16525100785e72ea06e46447898e3c7f815e
QooraPL <mikiotwock@gmail.com>
2025-01-11 13:40:19 +0100

    Update SpacesToDashes.java

--- src/SpacesToDashes.java
+++ src/SpacesToDashes.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.*;
 
-public class SpacesToDashes {
+public class SpacesToDashes{
+    public static void main(String[] args) {
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
+            reader.lines()
+                    .map(line -> line.replace(" ", "-"))
+                    .forEach(line -> {
+                        try {
+                            writer.write(line);
+                            writer.newLine();
+                        } catch (IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    });
+
+            System.out.println("W pliku spacje zostaly zastapione myslnikami.");
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd: " + e.getMessage());
+        }
+    }
 }
